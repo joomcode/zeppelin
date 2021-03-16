1d21009b4f543723e614ebcf35ffd7389818bc4f
Vladimir Prus <vladimir.prus@gmail.com>
2021-03-16 18:52:32 +0000

    Update build script.

--- dev/build_joom_release.sh
+++ dev/build_joom_release.sh
@@ -3,4 +3,4 @@
 set -eo pipefail
 
 ./dev/change_scala_version.sh 2.11
-mvn package -Pbuild-distr -DskipTests -P-Phadoop-2.6 -pl '!beam,!hbase,!pig,!ignite,!kylin,!lens,!cassandra,!elasticsearch,!alluxio,!scio,!groovy,!sap,!java,!geode,!neo4j,!hazelcastjet,!submarine,!sparql,!mongodb,!ksql,!scalding,!flink' -am
+mvn package -Pbuild-distr -DskipTests -Pweb-angular -Phadoop-2.6 -pl !beam,!hbase,!pig,!jdbc,!file,!flink,!ignite,!kylin,!lens,!cassandra,!elasticsearch,!bigquery,!alluxio,!scio,!livy,!groovy,!sap,!java,!geode,!neo4j,!hazelcastjet,!submarine,!sparql,!mongodb,!ksql,!scalding -am
