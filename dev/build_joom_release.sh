1b47fb39c12c51ba9ab17410702ee6c3dbc2757e
Vladimir Prus <vladimir.prus@gmail.com>
2021-03-19 11:41:13 +0000

    Include new UI.

--- dev/build_joom_release.sh
+++ dev/build_joom_release.sh
@@ -3,4 +3,4 @@
 set -eo pipefail
 
 ./dev/change_scala_version.sh 2.11
-mvn package -Pbuild-distr -DskipTests -Phadoop-2.6 -Pscala-2.11 -pl '!beam,!hbase,!pig,!file,!flink,!ignite,!kylin,!lens,!cassandra,!elasticsearch,!bigquery,!alluxio,!scio,!livy,!groovy,!sap,!java,!geode,!neo4j,!hazelcastjet,!submarine,!sparql,!mongodb,!ksql,!scalding,!flink-cmd,!influxdb' -am
+mvn package -Pbuild-distr -DskipTests -Phadoop-2.6 -Pscala-2.11 -Pweb-angular -pl '!beam,!hbase,!pig,!file,!flink,!ignite,!kylin,!lens,!cassandra,!elasticsearch,!bigquery,!alluxio,!scio,!livy,!groovy,!sap,!java,!geode,!neo4j,!hazelcastjet,!submarine,!sparql,!mongodb,!ksql,!scalding,!flink-cmd,!influxdb' -am
